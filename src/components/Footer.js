0c272871b59b5e8fcce1d4d9408b73367af28665
vedant dandge <vedantdandgepro@gmail.com>
2022-10-01 01:47:47 +0530

    added footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="footer mt-auto py-3 bg-light">
+            <div class="container">
+                <span class="text-muted">Made with ❤️ by Susmita-Dey.</span>
+            </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
