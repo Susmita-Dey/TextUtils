18b48579236283f6988dbd78040f98cde25e1453
Kumar Sanu <101383013+kumarsanu2002@users.noreply.github.com>
2022-10-06 19:54:22 +0530

    changed dark more color to proper dark
    I have changed color so that there is diff. b/w header and content part. I have also to change the footer color when dark mode is on ,I will make another pull request for that.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     // document.body.classList.add("bg-" + cls);
     if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = "#042743";
+      document.body.style.backgroundColor = "#191A19";
       showAlert("Dark mode has been enabled", "success");
 
       // document.title = "TextUtils - Dark Mode";
