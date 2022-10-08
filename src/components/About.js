9d0e29f21304a7faa15b39c6a6626e58e8285623
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2022-10-08 17:57:10 +0530

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,6 +12,13 @@ export default function About(props) {
       style={{ color: props.mode === "dark" ? "white" : "#042743" }}
     >
       <h1 className="my-3">About Us</h1>
+      <p><b>TextUtils</b> is a ReactJs website built primarily to do various operations on regular typed text.
+        You can safely use WordPad or NotePad for text drafting, and saving, but <i>TextUtils</i> offers much more than simple text drafting and formatting.
+        TextUtils can convert your text to any case in just one simple click of a button. It can extract links and numbers safely from a labyrinth of random text or sophisticated documentation.
+        It has an improved property of base64 encoding, reversal of your inputted text.You can even remove whitespaces from your scripted documents, and wear up your earphones to listen to it, instead of straining your eyes!
+        It does a detailed analyzing of your text, and provides an output of words, and characters, along with reading time as well. The best part of <i>TextUtils</i> is that it is an open source project, as a result, dozens 
+        of new features are in the upcoming, which definitely makes it a cut above the rest. <br/>The most eminent features of the website are as follows :
+      </p>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item" style={myStyle}>
           <h2 className="accordion-header" id="headingOne">
@@ -33,7 +40,7 @@ export default function About(props) {
             aria-labelledby="headingOne"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">Simple Text Analyzer</div>
+            <div className="accordion-body">It is a <i>Simple Text Analyzer</i> , which mutilates your text, but in a tender way</div>
           </div>
         </div>
         <div className="accordion-item" style={myStyle}>
@@ -57,7 +64,7 @@ export default function About(props) {
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body">
-              It is completely free to use. No credit cards required. Use as
+              It is completely <i>free</i> to use. No credit cards required. Use as
               much as you want!!
             </div>
           </div>
@@ -83,7 +90,7 @@ export default function About(props) {
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body">
-              It is compatible with all of your favorite browsers.
+              It is compatible with all of your <i>favorite</i> browsers.
             </div>
           </div>
         </div>
