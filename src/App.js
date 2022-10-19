f6d864b758d6fb892770fd6934a3b8de0ac93dcb
Vin <vinayakkumar2805@gmail.com>
2022-10-19 19:37:08 +0530

    colour palette

--- src/App.js
+++ src/App.js
@@ -12,38 +12,101 @@ function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  // const removeBodyClasses = () => {
-  //   document.body.classList.remove("bg-light");
-  //   document.body.classList.remove("bg-dark");
-  //   document.body.classList.remove("bg-success");
-  //   document.body.classList.remove("bg-danger");
-  //   document.body.classList.remove("bg-warning");
-  //   document.body.classList.remove("bg-primary");
-  // };
+    const toggleMode = (mode) => {
+      // dynamic chnages
+      // body
+      document.body.style.backgroundColor = "#fff"
+      document.body.style.color = "#000"
 
-  const toggleMode = () => {
-    // removeBodyClasses();
-    // console.log(cls);
-    // document.body.classList.add("bg-" + cls);
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enabled", "success");
+      
+      // navbar
+      Array.from(document.getElementById("nav").classList).forEach(element => {
+        document.getElementById("nav").classList.remove(element)
+      });
+      document.getElementById("nav").classList.add("navbar-"+mode, "bg-"+mode, "navbar", "navbar-expand-lg")
+      Array.from(document.querySelectorAll(".nav-link")).forEach(element => {
+        element.style.color = "#fff"
+      });
+      
+      
+      // buttons
+      Array.from(document.querySelectorAll(".btn")).forEach(element => {
+        element.style.backgroundColor = ""
+        element.style.color = "#fff"
+        Array.from(element.classList).forEach(e => {
+          element.classList.remove(e)
+          element.classList.add("btn", "mx-1", "my-1","btn-"+mode)
+        });
+      });
 
-      // document.title = "TextUtils - Dark Mode";
-      // setInterval(() => {
-      //   document.title = "TextUtils - Dark Mode";
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = "Install TextUtils  now";
-      // }, 1500);
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled", "success");
-      // document.title = "TextUtils - Light Mode";
-    }
-  };
+
+      // footer
+      Array.from(document.getElementById("footer").classList).forEach(element => {
+        document.getElementById("footer").classList.remove(element)
+      });
+      document.getElementById("footer").classList.add("footer", "mt-auto", "py-3", "bg-"+mode)
+      document.getElementById("footer").style.color = "#fff"
+      
+      
+      
+      // changes particular to each mode
+      if(mode==="dark"){
+        document.body.style.backgroundColor = "#042743"
+        document.body.style.color = "#fff"
+        document.getElementById("myBox").style.backgroundColor = "rgb(19, 70, 110)"
+        document.getElementById("myBox").style.color = "#fff"
+        Array.from(document.querySelectorAll(".btn")).forEach(element => {
+          element.style.backgroundColor = "rgb(19, 70, 110)"
+          element.style.color = "#fff"
+        });
+      }
+      
+      
+      if(mode==="light"){
+        document.getElementById("myBox").style.backgroundColor = "#fff"
+        document.getElementById("myBox").style.color = "#000"
+        Array.from(document.querySelectorAll(".nav-link")).forEach(element => {
+          element.style.color = "#000"
+        });
+        Array.from(document.querySelectorAll(".btn")).forEach(element => {
+          element.style.backgroundColor = "#fff"
+          element.style.color = "#000"
+        });
+        document.getElementById("footer").style.color = "#000"
+      }
+      
+      
+      if(mode==="primary"){
+        document.getElementById("myBox").style.backgroundColor = "blue"
+        document.getElementById("myBox").style.color = "#fff"
+      }
+      
+      
+      if(mode==="danger"){
+        document.getElementById("myBox").style.backgroundColor = "#d9534f"
+        document.getElementById("myBox").style.color = "#fff"
+      }
+      
+      
+      
+      if(mode==="success"){
+        document.getElementById("myBox").style.backgroundColor = "#5cb85c"
+        document.getElementById("myBox").style.color = "#fff"
+      }
+      
+      
+      if(mode==="warning"){
+        document.getElementById("myBox").style.backgroundColor = "rgb(255, 193, 7)"
+        document.getElementById("myBox").style.color = "#000"
+        Array.from(document.querySelectorAll(".btn")).forEach(element => {
+          element.style.color = "#000"
+        });
+        Array.from(document.querySelectorAll(".nav-link")).forEach(element => {
+          element.style.color = "#000"
+        });
+        document.getElementById("footer").style.color = "#000"
+      }
+    };
 
   const showAlert = (message, type) => {
     setAlert({
