6e140426039c3e3f586b750c631e47463f43c5df
SahilDahekar <dahekar30sahil@gmail.com>
2022-10-25 22:37:26 +0530

    Added switching dark mode for footer and removed unecessary spaces

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
 import { AiOutlineGithub } from 'react-icons/ai';
+import { useSelector} from 'react-redux';
 
-export class Footer extends Component {
-  render() {
-    let styles = {
-      fontSize: '2em',
-      color: 'black',
-      marginLeft: '.9rem',
-    };
+const Footer = () =>{
+    const themeState = useSelector((state) => state.theme);
     return (
-      <div>
-        <footer className="footer mt-auto py-3 bg-light">
+      <>
+        <footer className={themeState.mode === "light" ? "footer mt-auto py-2 bg-light text-muted" : "footer mt-auto py-2 bg-dark text-white"} style={{height: "2.7rem"}}>
           <div className="container text-center">
-            <span className="text-muted">
-              Made with ❤️ by Susmita-Dey.
-              <a style={styles} href="https://github.com/Susmita-Dey/TextUtils">
-                <AiOutlineGithub />
-              </a>
-            </span>
+            <p style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+              Made with ❤️ by <a className={themeState.mode === "light" ? "text-muted ms-1" : "text-white ms-1"} href="https://github.com/Susmita-Dey/TextUtils" target="_blank">Susmita-Dey</a> <a className={themeState.mode === "light" ? "text-black ms-2" : "text-white ms-2"} href="https://github.com/Susmita-Dey/TextUtils" target="_blank"><AiOutlineGithub size={25}/></a>
+            </p>
           </div>
         </footer>
-      </div>
+      </>
     );
-  }
 }
 
 export default Footer;
